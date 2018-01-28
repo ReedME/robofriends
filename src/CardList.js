bdcde5568584d1d8d457a595739b3c4d51d711a2
Brendan Reed <brendanreed@Brendans-iMac.local>
2018-01-28 16:05:11 +1100

    refactored some code

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  const cardComponent = robots.map((user, i) => {
-    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} username={robots[i].username} />
+  const cardArray = robots.map((user, i) => {
+    return (<Card
+      key={robots[i].id}
+      id={robots[i].id}
+      name={robots[i].name}
+      email={robots[i].email}
+      username={robots[i].username}
+            />
+          );
   })
   return(
     <div>
-      {cardComponent}
+      {cardArray}
     </div>
   );
 }
