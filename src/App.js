64a131fb8154dde355d984e178a3307264baf53e
Brendan Reed <brendanreed@Brendans-iMac.local>
2018-01-28 16:44:46 +1100

    having issue with searchbox filtering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () =>{
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+//use arrow function here so this remains property of App
+  onSearchChange = (event) => {
+    // need to use this
+    this.setState({ searchfield: event.target.value })
+
+  }
+
+  render (){
+    const filteredRobots = this.state.robots.filter(robot =>{
+    return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+  })
   return(
-    <CardList robots={robots} />
-  );
+    <div className='tc'>
+      <h1>RoboFriends Contacts List</h1>
+      <SearchBox searchChange={this.onSearchChange}/>
+      <CardList robots={filteredRobots} />
+    </div>
+      );
 }
+}
+
 
 export default App
